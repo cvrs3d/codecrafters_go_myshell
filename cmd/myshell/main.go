6e3753f1ccf0ad558b2be3a72e6d4b9845dba6ca
Yevgeniy Ustinov <ev.ustinov03@gmail.com>
2024-12-11 19:16:49 +0100

    2

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,18 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+
 
 	// Wait for user input
 	reader := bufio.NewReader(os.Stdin)
 
-	usrInput, err := reader.ReadString('\n')
-	if err != nil {
-	    fmt.Fprint(os.Stdout, "invalid_command: not found\n")
+    for {
+        fmt.Fprint(os.Stdout, "$ ")
+        usrInput, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Fprint(os.Stdout, "invalid_command: not found\n")
+        }
+        usrInput = usrInput[:len(usrInput) - 1]
+        fmt.Fprintf(os.Stdout, "%s: not found\n", usrInput)
 	}
-    usrInput = usrInput[:len(usrInput) - 1]
-	fmt.Fprintf(os.Stdout, "%s: not found\n", usrInput)
 }
