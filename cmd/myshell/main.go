45b834411c393b3b9ef04be5484a90788dc363f3
Yevgeniy Ustinov <ev.ustinov03@gmail.com>
2024-12-12 14:16:12 +0100

    5

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,10 +5,35 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"errors"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
+// type command
+func isBuiltin(command string) bool {
+    builtins := map[string]bool{
+        "echo": true,
+        "type": true,
+        "exit": true,
+    }
+    return builtins[command]
+}
+
+// Parse type <command>
+func handleTypeCmd(input string) error {
+    parts := strings.SplitN(input, " ", 2)
+    if len(parts) != 2 {
+        return errors.New("type: missing operand")
+    }
+
+    command := parts[1]
+
+    if isBuiltin(command) {
+        fmt.Fprintf(os.Stdout, "%s is a shell builtin\n")
+    } else {
+        fmt.Fprintf(os.Stdout, "%s: not found\n")
+    }
+    return nil
+}
 
 func main() {
 
@@ -23,9 +48,18 @@ func main() {
             fmt.Fprint(os.Stdout, "invalid_command: not found\n")
             continue
         }
+
         usrInput = strings.TrimSpace(usrInput)
+
         if usrInput == "exit 0"{
             break
+        }
+
+        if strings.HasPrefix(usrInput, "type ") {
+            err := handleTypeCmd(usrInput)
+            if err == nil {
+                continue
+            }
         }
 
         if strings.HasPrefix(usrInput, "echo ") {
